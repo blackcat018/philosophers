3c2892ffa9b8d1bfa4110783af4c54053d54dd19
Mohamed El Idrissi <moel-idr@c2r9p2.1337.ma>
2025-09-27 04:27:33 +0100

    mandatory polish

--- mandatory/philos/philos.c
+++ mandatory/philos/philos.c
@@ -6,7 +6,7 @@
 /*   By: moel-idr <moel-idr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 17:15:14 by moel-idr          #+#    #+#             */
-/*   Updated: 2025/09/26 22:06:48 by moel-idr         ###   ########.fr       */
+/*   Updated: 2025/09/27 04:26:49 by moel-idr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,43 +24,70 @@ int simulation_ended(t_data *data)
 void *monitor_routine(void *arg)
 {
     t_data *data = (t_data *)arg;
-    int i = 0;
+    int i;
+
     while (!simulation_ended(data))
     {
-        usleep(1000); // Check every 1ms
-        pthread_mutex_lock(&data->meal_mutex);
-        long last_meal = data->philos[i].last_meal_time;
-        pthread_mutex_unlock(&data->meal_mutex);
-        long current_time = get_time_in_ms(data);
-        if (current_time - last_meal > data->time_to_die)
+        usleep(1000);
+        i = 0;
+        while (i < data->philo_num && !simulation_ended(data))
         {
-            pthread_mutex_lock(&data->end_mutex);
-            data->simulation_end = 1;
-            pthread_mutex_unlock(&data->end_mutex);
+            pthread_mutex_lock(&data->meal_mutex);
+            long last_meal = data->philos[i].last_meal_time;
+            pthread_mutex_unlock(&data->meal_mutex);
+            long current_time = get_time_in_ms(data);
+            if (current_time - last_meal > data->time_to_die)
+            {
+                pthread_mutex_lock(&data->end_mutex);
+                data->simulation_end = 1;
+                pthread_mutex_unlock(&data->end_mutex);
 
-            pthread_mutex_lock(&data->print_mutex);
-            printf("%ld %d died\n", current_time, data->philos[i].id + 1);
-            pthread_mutex_unlock(&data->print_mutex);
-            return NULL;
+                pthread_mutex_lock(&data->print_mutex);
+                printf("%ld %d died\n", current_time, data->philos[i].id + 1);
+                pthread_mutex_unlock(&data->print_mutex);
+                return NULL;
+            }
+            i++;
         }
-        i++;
-        if (i >= data->philo_num)
+        if (data->must_eat_count > 0)
+        {
+            int all_full = 1;
             i = 0;
+            while (i < data->philo_num)
+            {
+                pthread_mutex_lock(&data->meal_mutex);
+                if (data->philos[i].meals_eaten < data->must_eat_count)
+                    all_full = 0;
+                pthread_mutex_unlock(&data->meal_mutex);
+                i++;
+            }
+            if (all_full)
+            {
+                pthread_mutex_lock(&data->end_mutex);
+                data->simulation_end = 1;
+                pthread_mutex_unlock(&data->end_mutex);
+                return NULL;
+            }
+        }
     }
     return NULL;
 }
+
 void *philo_routine(void *arg)
 {
     t_philo *philo = (t_philo *)arg;
-    if (philo->id % 2 == 0) {
+    if (philo->id % 2 == 0)
+	{
         while (!simulation_ended(philo->data))
 		{
             philo_eat(philo);
             philo_sleep(philo, philo->data->time_to_sleep);
             philo_think(philo);
         }
-    } else {
-        while (!simulation_ended(philo->data)) {
+    } else
+	{
+        while (!simulation_ended(philo->data))
+		{
             philo_sleep(philo, philo->data->time_to_sleep);
 			philo_think(philo);
             philo_eat(philo);
